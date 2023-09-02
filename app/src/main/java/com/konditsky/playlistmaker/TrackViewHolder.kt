2cb1d92bcd143ddec5ec686406477f7c245b3ea8
KailarStern <kailarstern@mail.ru>
2023-09-02 11:00:15 +0300

    Commit1.2.2

--- app/src/main/java/com/konditsky/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/konditsky/playlistmaker/TrackViewHolder.kt
@@ -5,6 +5,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 class TrackViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val trackNameTextView: TextView = view.findViewById(R.id.textTrackName)
@@ -18,6 +21,8 @@ class TrackViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         trackTimeTextView.text = track.trackTime
         Glide.with(albumImageView.context)
             .load(track.artworkUrl100)
+            .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(8)))
+            .placeholder(R.drawable.ic_mock)
             .into(albumImageView)
     }
 }
